c098519d6710a0c3028de6849ede2af07f9b16b7
hiphop5782 <zdlsz@hanmail.net>
2026-06-30 17:17:10 +0900

    state를 이용한 화면 제어

--- reactjs/demo03/src/App.jsx
+++ reactjs/demo03/src/App.jsx
@@ -8,12 +8,21 @@ function App() {
   //state : 화면의 근원이 되는 데이터
   //- 생성 : const [변수명, 세터함수명] = useState(초기값);
   const [name, setName] = useState("피카츄");
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>Hello~ {name}!</h1>
       <button onClick={()=>setName("피카츄")}>피카츄</button>
       <button onClick={()=>setName("라이츄")}>라이츄</button>
+
+      {/* 모든 태그는 종료 표시가 있어야 한다(input, img, hr, ...) */}
+      <hr/>
+
+      <h1>버튼을 한번 눌러보세요!</h1>
+      <button onClick={()=>setCount(count+1)}>클릭!</button>
+      <button onClick={()=>setCount(0)}>처음부터</button>
+      <h3>버튼 누른 횟수 : {count}번</h3>
     </>
   )
 }
