154badae42e13b0ec6b448441a0a3d4f8579fbbd
hiphop5782 <zdlsz@hanmail.net>
2026-07-01 15:14:25 +0900

    프로젝트 생성 및 컴포넌트(점보트론) 생성

---
+++ reactjs/demo09component/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import "./components/Jumbotron"//.jsx 생략
+
+function App() {
+
+  return (
+  <div className="container my-5">
+
+    {/* 내가 만든 점보트론을 불러와서 적용 */}
+    <Jumbotron/>
+
+  </div>
+  )
+}
+
+export default App
---
+++ reactjs/demo09component/src/components/Jumbotron.jsx
@@ -0,0 +1,19 @@
+//import
+
+//function
+function Jumbotron() {
+
+    return (
+    <div className="row">
+        <div className="col">
+            <div className="p-4 bg-dark text-light rounded">
+                <h1>제목</h1>
+                <p>내용설명</p>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+//export
+export default Jumbotron;
