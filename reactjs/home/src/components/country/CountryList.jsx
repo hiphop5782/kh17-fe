d183386d07f7082deaa27040de9f29b89995d6e3
hiphop5782 <zdlsz@hanmail.net>
2026-07-07 16:48:22 +0900

    국가 목록 및 등록을 신규 주소 체계에 맞게 변경

--- reactjs/home/src/components/country/CountryList.jsx
+++ reactjs/home/src/components/country/CountryList.jsx
@@ -19,7 +19,7 @@ export default function CountryList() {
     }, []);
     
     //callback
-    const loadMoreList = useCallback(()=>{
+    const loadMoreList = useCallback(async ()=>{
         //이미 로딩중이면 차단
         if(loading === true) return;
         setLoading(true);
@@ -27,21 +27,17 @@ export default function CountryList() {
         const dataSize = countryList.length;
         const lastCountryNo = dataSize === 0 ? 0 : countryList[dataSize-1].countryNo;
 
-        axios({
-            url:"http://localhost:8080/api/country/listForReact",
-            method:"get",
-            params: {//GET방식일 때
-                lastCountryNo : lastCountryNo,
-                size : size
-            }
-        })
-        .then(response=>{
-            //덮어쓰기가 아니라 추가(이어쓰기)가 필요
-            //setCountryList(response.data.list);//덮어쓰기
-            setCountryList([...countryList, ...response.data.list]);//이어쓰기
-            setLast(response.data.last);
-        })
-        .finally(()=>setLoading(false));
+        // const response = await axios.get(
+        //     `http://localhost:8080/api/country/lastCountryNo/${lastCountryNo}/size/${size}`
+        // );
+        const response = await axios.post(
+            "http://localhost:8080/api/country/list-more",
+            { lastNo : lastCountryNo , size : size }
+        );
+        setCountryList([...countryList, ...response.data.list]);//이어쓰기
+        setLast(response.data.last);
+
+        setLoading(false);
     }, [countryList, size]);
 
     return (<>
