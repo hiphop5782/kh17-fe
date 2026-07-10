fa13925b9ee8c754cd933dd095f6ba7b4165870c
hiphop5782 <zdlsz@hanmail.net>
2026-07-10 16:41:27 +0900

    IME모드와 Composition Event로 처리하려다가 망했어요!

--- reactjs/home/src/templates/Body.jsx
+++ reactjs/home/src/templates/Body.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import BookList from "@components/book/BookList";
-import CountryList from "@components/country/CountryList";
-import LectureList from "@components/lecture/LectureList";
 import Home from "@components/Home";
 import NotFound from "@error/NotFound";
+
+import CountryList from "@components/country/CountryList";
 import CountryAdd from "@components/country/CountryAdd";
 import CountryDetail from "@components/country/CountryDetail";
+import CountryEdit from "@components/country/CountryEdit";
+import CountrySearch from "@components/country/CountrySearch";
+
+import LectureList from "@components/lecture/LectureList";
 import LectureAdd from "@components/lecture/LectureAdd";
 import LectureDetail from "@components/lecture/LectureDetail";
-import CountryEdit from "@components/country/CountryEdit";
+
+import BookList from "@components/book/BookList";
 import BookSpa from "@components/book/BookSpa";
 
 
@@ -23,6 +27,7 @@ export default function Body() {
         {/* 제일 마지막에 적혀있는 값을 countryNo라는 이름으로 관리하겠다 */}
         <Route path="/country/detail/:countryNo" element={<CountryDetail/>}/>
         <Route path="/country/edit/:countryNo" element={<CountryEdit/>}/>
+        <Route path="/country/search" element={<CountrySearch/>}/>
         
         <Route path="/lecture/list" element={<LectureList/>}/>
         <Route path="/lecture/add" element={<LectureAdd/>}/>
