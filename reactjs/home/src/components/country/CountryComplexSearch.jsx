be4d892eb152d449f9aca825f5f197d16ee1cc63
hiphop5782 <zdlsz@hanmail.net>
2026-07-13 12:51:03 +0900

    복합검색 화면 구현중

--- reactjs/home/src/components/country/CountryComplexSearch.jsx
+++ reactjs/home/src/components/country/CountryComplexSearch.jsx
@@ -8,7 +8,9 @@ import axios from "axios";
 export default function CountryComplexSearch() {
     //state
     const [condition, setCondition] = useState({
+        countryRegions : [],
         countryName : "",
+        countryCapital : "",
     });
 
     //callback
@@ -25,16 +27,38 @@ export default function CountryComplexSearch() {
         console.log(response.data);
     }, [condition]);
 
+    const checkCountryRegion = useCallback(e=>{
+        console.log(e.target.checked, e.target.value);
+    }, []);
+
     //view
     return (<>
         <Jumbotron title="국가 복합 검색 예제" content="조건이 있을지 없을지 모르는 형태를 처리해봅시다"/>
 
         {/* 검색 화면 */}
         <Row className="mt-4">
+            <Form.Label column sm={3}>대륙</Form.Label>
+            <Col sm={9}>
+                <Form.Check type="checkbox" value={"아시아"} label="아시아" onChange={checkCountryRegion} checked={condition.countryRegions.includes("아시아")}/>
+                <Form.Check type="checkbox" value={"아프리카"} label="아프리카" onChange={checkCountryRegion} checked={condition.countryRegions.includes("아프리카")}/>
+                <Form.Check type="checkbox" value={"북아메리카"} label="북아메리카" onChange={checkCountryRegion} checked={condition.countryRegions.includes("북아메리카")}/>
+                <Form.Check type="checkbox" value={"남아메리카"} label="남아메리카" onChange={checkCountryRegion} checked={condition.countryRegions.includes("남아메리카")}/>
+                <Form.Check type="checkbox" value={"오세아니아"} label="오세아니아" onChange={checkCountryRegion} checked={condition.countryRegions.includes("오세아니아")}/>
+                <Form.Check type="checkbox" value={"유럽"} label="유럽" onChange={checkCountryRegion} checked={condition.countryRegions.includes("유럽")}/>
+            </Col>
+        </Row>
+        <Row className="mt-4">
             <Form.Label column sm={3}>국가명</Form.Label>
             <Col sm={9}>
                 <Form.Control type="text" value={condition.countryName}
                         name="countryName" onChange={changeStringValue}/>
+            </Col>
+        </Row>
+        <Row className="mt-4">
+            <Form.Label column sm={3}>수도명</Form.Label>
+            <Col sm={9}>
+                <Form.Control type="text" value={condition.countryCapital}
+                        name="countryCapital" onChange={changeStringValue}/>
             </Col>
         </Row>
 
