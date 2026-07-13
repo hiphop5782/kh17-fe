f6bb28a63f09748fcb00673f44995f05e0c39537
hiphop5782 <zdlsz@hanmail.net>
2026-07-13 14:54:04 +0900

    복합검색 화면 구현중

--- reactjs/home/src/components/country/CountryComplexSearch.jsx
+++ reactjs/home/src/components/country/CountryComplexSearch.jsx
@@ -2,8 +2,11 @@ import Jumbotron from "@templates/Jumbotron"
 import { useCallback } from "react";
 import { useState } from "react"
 import { Button, Col, Form, Row } from "react-bootstrap"
-import { FaMagnifyingGlass } from "react-icons/fa6"
+import { FaMagnifyingGlass, FaPlus } from "react-icons/fa6"
+import { TbTilde } from "react-icons/tb";
 import axios from "axios";
+import { useMemo } from "react";
+import { toast } from "react-toastify";
 
 export default function CountryComplexSearch() {
     //state
@@ -11,6 +14,13 @@ export default function CountryComplexSearch() {
         countryRegions : [],
         countryName : "",
         countryCapital : "",
+        minCountryPopulation: "",
+        maxCountryPopulation: "",
+        size:10,
+        orders:[
+            // "country_population desc",
+            // "country_name asc",
+        ]
     });
 
     //callback
@@ -21,6 +31,14 @@ export default function CountryComplexSearch() {
             [name] : value
         }));
     }, []);
+    const changeNumericValue = useCallback(e=>{
+        const { name, value } = e.target;
+        const replacement = value.replace(/[^0-9]+/g, "");
+        setCondition(prev=>({
+            ...prev,
+            [name] : replacement
+        }));
+    }, []);
 
     const send = useCallback(async ()=>{
         const response = await axios.post("/api/country/complexSearch", condition);
@@ -28,8 +46,42 @@ export default function CountryComplexSearch() {
     }, [condition]);
 
     const checkCountryRegion = useCallback(e=>{
-        console.log(e.target.checked, e.target.value);
+        //console.log(e.target.checked, e.target.value);
+        //true면 추가, false면 제거
+        const { checked, value } = e.target;
+
+        const clone = [...condition.countryRegions];
+        const clone2 = checked ? [...clone, value] : clone.filter(region=> region !== value);
+
+        setCondition({ ...condition, countryRegions:clone2 });
+    }, [condition]);
+
+    //state는 변경이 가능하므로 변경 불가능한건 memo로 생성할 수도 있다
+    //const [orderList, setOrderList] = useState(["..."]);
+    const orderList = useMemo(()=>{
+        return [
+            "country_name asc",
+            "country_population desc",
+            "country_capital asc"
+        ];
     }, []);
+
+    //선택 가능한 목록 : orderList의 항목 중에 condition.orders에 없는 요소들만 검색하여 반환
+    const availableOrderList = useMemo(()=>{
+        return orderList.filter(order=>!condition.orders.includes(order));
+    }, [orderList, condition.orders]);
+
+    //condition 중에서 orders에 한 개의 데이터를 추가
+    const addOrder = useCallback(e=>{
+        if(availableOrderList.length === 0){
+            toast.error("더 이상 추가가 불가능합니다");
+            return;
+        }
+
+        const clone = [...condition.orders];
+        const clone2 = [...clone, availableOrderList[0]];
+        setCondition({...condition, orders : clone2});
+    }, [condition, availableOrderList]);
 
     //view
     return (<>
@@ -61,6 +113,50 @@ export default function CountryComplexSearch() {
                         name="countryCapital" onChange={changeStringValue}/>
             </Col>
         </Row>
+        <Row className="mt-4">
+            <Form.Label column sm={3}>인구수</Form.Label>
+            <Col sm={9}>
+                <div className="d-flex align-items-center">
+                    <Form.Control type="text" name="minCountryPopulation"
+                            value={condition.minCountryPopulation}
+                            onChange={changeNumericValue}/>
+                    <TbTilde className="mx-2" size={30}/>
+                    <Form.Control type="text" name="maxCountryPopulation"
+                            value={condition.maxCountryPopulation}
+                            onChange={changeNumericValue}/>
+                </div>
+            </Col>
+        </Row>
+        <Row className="mt-4">
+            <Form.Label column sm={3}>검색 개수</Form.Label>
+            <Col sm={9}>
+                <Form.Select name="size" value={condition.size} onChange={changeNumericValue}>
+                    <option value={""}>한번에 보기</option>
+                    <option value={10}>10개씩 보기</option>
+                    <option value={20}>20개씩 보기</option>
+                    <option value={50}>50개씩 보기</option>
+                    <option value={100}>100개씩 보기</option>
+                </Form.Select>
+            </Col>
+        </Row>
+        <Row className="mt-4">
+            <Form.Label column sm={3} className="d-inline-flex align-items-center">
+                <span>정렬 방식</span>
+                <FaPlus className="ms-2 text-info" onClick={addOrder}/>
+            </Form.Label>
+            <Col sm={9}>
+                {condition.orders.length === 0 ? (
+                <span>기본값(번호순)</span>
+                ) : (<>
+                {/* condition.orders의 내용을 반복적으로 출력 */}
+                <ul>
+                    {condition.orders.map((order, index)=>(
+                    <li key={index}>{order}</li>
+                    ))}
+                </ul>
+                </>)}
+            </Col>
+        </Row>        
 
         <Row className="mt-4">
             <Col>
