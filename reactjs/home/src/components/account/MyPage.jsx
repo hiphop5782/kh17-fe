66f4b8a5dafc18c8fc931cf108f1370b86b57c26
hiphop5782 <zdlsz@hanmail.net>
2026-07-20 12:49:58 +0900

    내 정보 불러와서 출력하기

--- reactjs/home/src/components/account/MyPage.jsx
+++ reactjs/home/src/components/account/MyPage.jsx
@@ -1,70 +1,94 @@
 import Jumbotron from "@templates/Jumbotron";
+import { useAtom, useAtomValue } from "jotai";
 import { Col, Row } from "react-bootstrap";
+import { loginUserState } from "@utils/storage";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
 export default function MyPage() {
+    //jotai state에 저장된 내 정보를 가져와서 서버에 나머지 정보를 요청해야함
+    //const [loginUser, setLoginUser] = useAtom(loginUserState);
+    //const loginUser = useAtomValue(loginUserState);
+    const { accountId, accountNickname, accoutLevel } = useAtomValue(loginUserState);
 
+    const [ account, setAccount ] = useState(null);
+
+    useEffect(()=>{
+        loadData();
+    }, []);
+
+    const loadData = useCallback(async ()=>{
+        const {data} = await axios.get(`/api/account/${accountId}`);
+        setAccount(data);
+    }, [accountId]);
 
     return (<>
         <Jumbotron title="???님의 개인 정보"/>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">아이디</Col>
+            <Col sm={9} className="text-secondary">{account?.accountId}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">닉네임</Col>
+            <Col sm={9} className="text-secondary">{account?.accountNickname}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">이메일</Col>
+            <Col sm={9} className="text-secondary">{account?.accountEmail}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">생년월일</Col>
+            <Col sm={9} className="text-secondary">{account?.accountBirth}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">연락처</Col>
+            <Col sm={9} className="text-secondary">{account?.accountContact}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">주소</Col>
+            <Col sm={9} className="text-secondary">
+                [{account?.accountPost}]
+                {account?.accountAddress1}
+                {account?.accountAddress2}
+            </Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">등급</Col>
+            <Col sm={9} className="text-secondary">{account?.accountLevel}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">포인트</Col>
+            <Col sm={9} className="text-secondary">
+                {account?.accountPoint.toLocaleString()} point
+            </Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">가입일</Col>
+            <Col sm={9} className="text-secondary">{account?.accountJoin}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">최종로그인</Col>
+            <Col sm={9} className="text-secondary">{account?.accountLogin}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">최종변경일</Col>
+            <Col sm={9} className="text-secondary">{account?.accountChange}</Col>
         </Row>
 
         <Row className="mt-4">
-            <Col sm={3} className="fw-bold text-info">항목</Col>
-            <Col sm={9} className="text-secondary">데이터</Col>
+            <Col sm={3} className="fw-bold text-info">상태메세지</Col>
+            <Col sm={9} className="text-secondary">{account?.accountMessage}</Col>
         </Row>
     </>)
 }
