07cf0b45ff078cc8eead9f6db06e2ed01aa9b069
hiphop5782 <zdlsz@hanmail.net>
2026-07-23 17:22:13 +0900

    결과 메세지 출력까지 구현 완료

--- reactjs/home/src/components/account/AccountChange.jsx
+++ reactjs/home/src/components/account/AccountChange.jsx
@@ -372,14 +372,23 @@ export default function AccountChange() {
         try {
             const copy = {...account};
             const { data } = await apiClient.put("/account/", copy);
-            console.log(data);
-            //toast.success("회원 가입이 완료되었습니다");
-            //navigate("/account/joinSuccess");
+            
+            if(data.status === true) {
+                toast.success(data.message);
+                navigate("/account/mypage");
+            }
+            else {
+                toast.error(data.message);
+            }
         }
         catch(e) {
-            console.error(e);
-            //toast.error("회원 가입 과정에서 오류가 발생했습니다");
-            //navigate("/account/joinFail");
+            //console.error(e);
+            await Swal.fire({
+                title: "서버 오류 발생",
+                text: "잠시 후 다시 시도해주세요",
+                icon: "warning",
+                confirmButtonText: "확인",
+            });
         }
     }, [account]);
 
