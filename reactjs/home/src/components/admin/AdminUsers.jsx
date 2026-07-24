ed6e719db8abd0bb5549ac75e01c7fa26b712266
hiphop5782 <zdlsz@hanmail.net>
2026-07-24 17:51:29 +0900

    복합 검색 구현(화면)

--- reactjs/home/src/components/admin/AdminUsers.jsx
+++ reactjs/home/src/components/admin/AdminUsers.jsx
@@ -3,6 +3,15 @@ import { useCallback, useState } from "react"
 import { Button, Col, Form, Row, Table } from "react-bootstrap";
 import { FaEraser, FaMagnifyingGlass } from "react-icons/fa6";
 import { apiClient } from "@utils/reaxios";
+import { TbTilde } from "react-icons/tb";
+
+import { ko } from "date-fns/locale";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+dayjs.locale("ko");//한국어로 설정
 
 export default function AdminUsers() {
     //state
@@ -15,6 +24,7 @@ export default function AdminUsers() {
         accountBirthBegin : "", accountBirthEnd : "",
         accountJoinBegin : "", accountJoinEnd : "",
         accountLoginBegin : "", accountLoginEnd : "",
+        accountPointMin : "", accountPointMax : "",
         accountLevels : [],
         accountBlock : ""
     });
@@ -23,6 +33,15 @@ export default function AdminUsers() {
         setCondition(prev=>({
             ...prev,
             [name] : value
+        }));
+    }, []);
+    const changeNumericValue = useCallback(e=>{
+        const { name, value } = e.target;
+        const replacement = value.replace(/[^0-9]+/g, "");
+        const replacement2 = parseInt(replacement) || "";
+        setCondition(prev=>({
+            ...prev,
+            [name] : replacement2
         }));
     }, []);
 
@@ -84,6 +103,181 @@ export default function AdminUsers() {
                 <Form.Control type="text" name="accountAddress"
                     value={condition.accountAddress} onChange={changeStringValue}
                     placeholder="일부분만 일치해도 검색"/>
+            </Col>
+        </Row>
+
+        <Row className="mt-2">
+            <Form.Label column sm={3}>생년월일</Form.Label>
+            <Col sm={9}>
+                <div className="d-flex align-items-center">
+                    <DatePicker 
+                        name="accountBirthBegin" 
+                        selected={condition.accountBirthBegin}
+                        onChange={(date)=>{
+                            //date가 우리가 원하는 형식이 아님(내일 변경 후 설정)
+                            //→ dayjs를 이용해서 "YYYY-MM-DD" 형태로 변경
+                            const convertDate = dayjs(date).format("YYYY-MM-DD");
+                            setCondition(prev=>({...prev, accountBirthBegin: convertDate}))
+                        }}
+                        dateFormat={"yyyy-MM-dd"}
+                        customInput={<Form.Control/>}
+                        wrapperClassName={`w-100`}
+                        
+                        showYearDropdown
+                        showMonthDropdown
+                        dropdownMode="select"
+
+                        locale={ko}
+                    />
+                    <TbTilde size={24} className="mx-2"/>
+                    <DatePicker 
+                        name="accountBirthEnd" 
+                        selected={condition.accountBirthEnd}
+                        onChange={(date)=>{
+                            //date가 우리가 원하는 형식이 아님(내일 변경 후 설정)
+                            //→ dayjs를 이용해서 "YYYY-MM-DD" 형태로 변경
+                            const convertDate = dayjs(date).format("YYYY-MM-DD");
+                            setCondition(prev=>({...prev, accountBirthEnd: convertDate}))
+                        }}
+                        dateFormat={"yyyy-MM-dd"}
+                        customInput={<Form.Control/>}
+                        wrapperClassName={`w-100`}
+                        
+                        showYearDropdown
+                        showMonthDropdown
+                        dropdownMode="select"
+
+                        locale={ko}
+                    />
+                </div>
+            </Col>
+        </Row>
+        <Row className="mt-2">
+            <Form.Label column sm={3}>가입일</Form.Label>
+            <Col sm={9}>
+                <div className="d-flex align-items-center">
+                    <DatePicker 
+                        name="accountJoinBegin" 
+                        selected={condition.accountJoinBegin}
+                        onChange={(date)=>{
+                            //date가 우리가 원하는 형식이 아님(내일 변경 후 설정)
+                            //→ dayjs를 이용해서 "YYYY-MM-DD" 형태로 변경
+                            const convertDate = dayjs(date).format("YYYY-MM-DD");
+                            setCondition(prev=>({...prev, accountJoinBegin: convertDate}))
+                        }}
+                        dateFormat={"yyyy-MM-dd"}
+                        customInput={<Form.Control/>}
+                        wrapperClassName={`w-100`}
+                        
+                        showYearDropdown
+                        showMonthDropdown
+                        dropdownMode="select"
+
+                        locale={ko}
+                    />
+                    <TbTilde size={24} className="mx-2"/>
+                    <DatePicker 
+                        name="accountJoinEnd" 
+                        selected={condition.accountJoinEnd}
+                        onChange={(date)=>{
+                            //date가 우리가 원하는 형식이 아님(내일 변경 후 설정)
+                            //→ dayjs를 이용해서 "YYYY-MM-DD" 형태로 변경
+                            const convertDate = dayjs(date).format("YYYY-MM-DD");
+                            setCondition(prev=>({...prev, accountJoinEnd: convertDate}))
+                        }}
+                        dateFormat={"yyyy-MM-dd"}
+                        customInput={<Form.Control/>}
+                        wrapperClassName={`w-100`}
+                        
+                        showYearDropdown
+                        showMonthDropdown
+                        dropdownMode="select"
+
+                        locale={ko}
+                    />
+                </div>
+            </Col>
+        </Row>
+        <Row className="mt-2">
+            <Form.Label column sm={3}>최종로그인</Form.Label>
+            <Col sm={9}>
+                <div className="d-flex align-items-center">
+                    <DatePicker 
+                        name="accountLoginBegin" 
+                        selected={condition.accountLoginBegin}
+                        onChange={(date)=>{
+                            //date가 우리가 원하는 형식이 아님(내일 변경 후 설정)
+                            //→ dayjs를 이용해서 "YYYY-MM-DD" 형태로 변경
+                            const convertDate = dayjs(date).format("YYYY-MM-DD");
+                            setCondition(prev=>({...prev, accountLoginBegin: convertDate}))
+                        }}
+                        dateFormat={"yyyy-MM-dd"}
+                        customInput={<Form.Control/>}
+                        wrapperClassName={`w-100`}
+                        
+                        showYearDropdown
+                        showMonthDropdown
+                        dropdownMode="select"
+
+                        locale={ko}
+                    />
+                    <TbTilde size={24} className="mx-2"/>
+                    <DatePicker 
+                        name="accountLoginEnd" 
+                        selected={condition.accountLoginEnd}
+                        onChange={(date)=>{
+                            //date가 우리가 원하는 형식이 아님(내일 변경 후 설정)
+                            //→ dayjs를 이용해서 "YYYY-MM-DD" 형태로 변경
+                            const convertDate = dayjs(date).format("YYYY-MM-DD");
+                            setCondition(prev=>({...prev, accountLoginEnd: convertDate}))
+                        }}
+                        dateFormat={"yyyy-MM-dd"}
+                        customInput={<Form.Control/>}
+                        wrapperClassName={`w-100`}
+                        
+                        showYearDropdown
+                        showMonthDropdown
+                        dropdownMode="select"
+
+                        locale={ko}
+                    />
+                </div>
+            </Col>
+        </Row>
+
+
+        <Row className="mt-2">
+            <Form.Label column sm={3}>포인트</Form.Label>
+            <Col sm={9}>
+                <div className="d-flex align-items-center">
+                    <Form.Control type="text" inputMode="numeric"
+                        name="accountPointMin" value={condition.accountPointMin}
+                        onChange={changeNumericValue}
+                        placeholder="최소 포인트"/>
+                    <TbTilde size={24} className="mx-2"/>
+                    <Form.Control type="text" inputMode="numeric"
+                        name="accountPointMax" value={condition.accountPointMax}
+                        onChange={changeNumericValue}
+                        placeholder="최대 포인트"/>
+                </div>
+            </Col>
+        </Row>
+
+        <Row className="mt-2">
+            <Form.Label column sm={3}>차단여부</Form.Label>
+            <Col sm={9}>
+                <Form.Check type="radio" label="전체" 
+                            name="accountBlock" value=""
+                            checked={condition.accountBlock === ""}
+                            onChange={e=>setCondition(prev=>({...prev, accountBlock:""}))}/>
+                <Form.Check type="radio" label="차단된 회원만" 
+                            name="accountBlock" value="Y"
+                            checked={condition.accountBlock === "Y"}
+                            onChange={e=>setCondition(prev=>({...prev, accountBlock:"Y"}))}/>
+                <Form.Check type="radio" label="차단되지 않은 회원만" 
+                            name="accountBlock" value="N"
+                            checked={condition.accountBlock === "N"}
+                            onChange={e=>setCondition(prev=>({...prev, accountBlock:"N"}))}/>
             </Col>
         </Row>
 
