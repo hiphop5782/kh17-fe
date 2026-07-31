fb987b67d526a1c635ec5e9e83c47c9b8a4d3082
hiphop5782 <zdlsz@hanmail.net>
2026-07-31 12:50:21 +0900

    썸네일 이미지 변경

--- reactjs/home/src/components/admin/sale/AdminSaleEdit.jsx
+++ reactjs/home/src/components/admin/sale/AdminSaleEdit.jsx
@@ -1,5 +1,5 @@
 import Jumbotron from "@templates/Jumbotron";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { apiClient } from "@utils/reaxios";
 import { Button, Col, Form, Row } from "react-bootstrap";
@@ -14,14 +14,14 @@ export default function AdminSaleEdit() {
 
     //state
     const [sale, setSale] = useState(null);
-    const [thumbnail, setThumbnail] = useState(null);
+    const [beforeThumbnail, setBeforeThumbnail] = useState(null);//AttachDto(DB정보)
     const [detailImages, setDetailImages] = useState([]);
 
     const loadData = useCallback(async ()=>{
         const { data } = await apiClient.get(`/sale/${saleNo}`);
         const { saleDto, thumbnail, details } = data;
         setSale(saleDto);
-        setThumbnail(thumbnail);
+        setBeforeThumbnail(thumbnail);
         setDetailImages(details);
         //할인 체크박스 처리 추가
         setDiscount(saleDto.saleOriginalPrice > saleDto.saleDiscountPrice);
@@ -73,6 +73,35 @@ export default function AdminSaleEdit() {
         const { data } = await apiClient.put(`/sale/${saleNo}`, form);
         console.log(data);
     }, [sale, discount]);
+
+    //썸네일(thumbnail) 관련 기능들
+    const [thumbnail, setThumbnail] = useState(null);//파일
+    const thumbnailRef = useRef();
+
+    //(+변경사항) 2023년 3월 이후로 취소버튼은 onchange, oninput으로 감지되지 않습니다.
+    const changeThumbnail = useCallback(async e=>{
+        //선택된 파일을 서버로 전송시켜서 진짜 이미지 변경을 시킨다(+업로드 & DB변경)
+        const file = e.target.files[0];
+
+        const form = new FormData();
+        form.append("thumbnail", file);
+        const { data } = await apiClient.patch(`/sale/thumbnail/${saleNo}`, form);
+
+        setThumbnail(file);
+        setBeforeThumbnail(data.attach);//변경된 이미지를 기존 이미지 정보에 덮어쓰기
+    }, []);
+    const clearThumbnail = useCallback(()=>{
+        setThumbnail(null);
+    }, []);
+    useEffect(()=>{
+        if(thumbnail !== null) return;
+
+        //파일선택창은 비어있는 value밖에 줄 수 없어서 리액트에서 모든 상황을 제어할 수 없다 (HTML보안 이슈)
+        //태그를 직접 제어하는 방향으로 우회 처리한다 (ref 사용)
+        if(thumbnailRef.current) {
+            thumbnailRef.current.value = "";
+        }
+    }, [thumbnail]);
 
 
     //sale은 절대로 null이면 안된다
@@ -155,6 +184,35 @@ export default function AdminSaleEdit() {
             </Col>
         </Row>
 
+        {/* 썸네일 이미지 표시 및 수정 */}
+        <Row className="mt-4">
+            <Form.Label column sm={3}>대표이미지</Form.Label>
+            <Col sm={9}>
+                <div className="d-flex">
+                    <Form.Control type="file" accept="image/*" 
+                        ref={thumbnailRef} 
+                        onInput={changeThumbnail}/>
+                    {thumbnail !== null && (
+                    <Button variant="danger" onClick={clearThumbnail} className="ms-2">
+                        <FaXmark/>                        
+                    </Button>
+                    )}
+                </div>
+            </Col>
+        </Row>
+        <Row className="mt-2">
+            <Col sm={{offset:3, span:9}}>
+                {/* 기존 이미지를 표시하고 제거, 변경 버튼을 추가 */}
+                {beforeThumbnail === null && (
+                <img src={NoImage} width={100} className="border"/>
+                ) }
+                {beforeThumbnail !== null && (
+                <img src={`${import.meta.env.VITE_SERVER_URL}/api/attach/${beforeThumbnail.attachNo}`} width={100} className="border"/>
+                ) }
+            </Col>
+        </Row>
+
+        {/* 수정버튼 */}
         <Row className="mt-5">
             <Col className="text-end">
                 <Button variant="success" size="lg" className="w-md-auto" 
