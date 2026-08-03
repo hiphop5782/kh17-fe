6d147af0aea32a69b2fbc88a3688718287ae11df
hiphop5782 <zdlsz@hanmail.net>
2026-08-03 09:18:15 +0900

    관리자 상품 상세 이미지 제거 기능 구현

--- reactjs/home/src/components/admin/sale/AdminSaleEdit.jsx
+++ reactjs/home/src/components/admin/sale/AdminSaleEdit.jsx
@@ -217,10 +217,16 @@ export default function AdminSaleEdit() {
         ).map(
             attach => attach.attachNo//전체 정보말고 번호만 추려라
         );
-        console.log(detailNumbers);
+        //console.log(detailNumbers);
 
         await apiClient.post(`/sale/deleteDetailImages/${saleNo}`, detailNumbers);
         //화면갱신
+        //- loadData는 안되고 화면에서 요소를 직접 제거해야함 (filter 사용)
+        toast.success("상세 이미지가 삭제되었습니다");
+        setBeforeDetailImages(prev=>prev.filter(
+            attach => !detailNumbers.includes(attach.attachNo)//지운 번호가 아닌 요소만 추출
+        ));
+
     }, [beforeDetailImages]);
 
     //sale은 절대로 null이면 안된다
