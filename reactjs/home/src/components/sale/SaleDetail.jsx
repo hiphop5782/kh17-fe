f060ae1ca2787a9d2728cef2632002181d9bb5d9
hiphop5782 <zdlsz@hanmail.net>
2026-08-10 09:57:00 +0900

    상품 수량 선택 시 드래그 되어있도록 조정

--- reactjs/home/src/components/sale/SaleDetail.jsx
+++ reactjs/home/src/components/sale/SaleDetail.jsx
@@ -183,6 +183,7 @@ export default function SaleDetail() {
                     {/* 수량 선택창과 구매버튼 */}
                     <Form.Control type="number" className="d-inline-block" 
                             style={{width:80}} value={quantity}
+                            onFocus={e=>e.target.select()}
                             onChange={e=>{
                                 let number = parseInt(e.target.value) || 1;
                                 if(number < 1) //최소값 보정 : number = Math.max(number, 1);
